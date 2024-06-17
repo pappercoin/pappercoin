a7630bafa69260153c6ff1ed9b8c231987db0b61
pappercoin <triz@bk.ru>
2024-06-17 19:39:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('YOUR_TELEGRAM_BOT_TOKEN');
+
+bot.start((ctx) => {
+    ctx.reply('Welcome to PepperCoin! Click the button below to open the app.', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Open PepperCoin App', web_app: { url: 'https://<твоё-имя-пользователя>.github.io/<имя-репозитория>' } }]
+            ]
+        }
+    });
+});
+
+bot.launch();
+
 let coinCount = 0;
 
 document.getElementById('clickButton').addEventListener('click', () => {
